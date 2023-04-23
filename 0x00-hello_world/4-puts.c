e9b258713c55d233ded336eeb204a633b16c4927
Ruky8821 <rumena78@gmail.com>
2023-04-23 06:20:47 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
+/**
+*main - entry point
+*return: 0 is always a success
+*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
