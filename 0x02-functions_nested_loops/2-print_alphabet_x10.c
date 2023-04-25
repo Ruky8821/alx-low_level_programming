0961b0931fdd6e4e2e2f4cac8994746040f26e36
Ruky8821 <rumena78@gmail.com>
2023-04-25 14:21:50 -0700

    letters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_alphabet_x10 - prints letters 10 times
+  */
+
+void print_alphabet_x10(void)
+{
+	int l, ch;
+
+	for (ch = 'a'; l <= 9; l++)
+		_putchar(ch);
+	_putchar ('\n');
+}
