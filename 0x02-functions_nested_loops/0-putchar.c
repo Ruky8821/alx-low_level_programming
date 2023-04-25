5e43c76f54746805adbb6926db734870870f0b2f
Ruky8821 <rumena78@gmail.com>
2023-04-25 13:40:34 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
  * main - Entry point
- *description: "prints without stdio"
- *Return: 0 success
+ * description: "prints without stdio"
+ * Return: 0 success
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('t');
+	char str[] = "_putchar";
+	int letters;
+
+	for (letters = 0; letters < 8; letters++)
+		_putchar(str[letters]);
 	_putchar('\n');
+
 	return (0);
 }
