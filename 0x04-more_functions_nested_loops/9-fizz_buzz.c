9b24702abcc8a548a805da8e1cea21482fb8f880
Ruky8821 <rumena78@gmail.com>
2023-04-28 05:52:38 -0700

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+			printf("fizz");
+		}
+		if (n % 5 == 0)
+		{
+			printf("buzz");
+		}
+		if ((n % 3 == 0) && (n & 5 == 0))
+		{
+			printf("fizzBuzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+	}
+	return (0);
+}
