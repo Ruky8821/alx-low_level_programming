80d6f24952e3b9c9061f8ae4aa91d8ded8d58e65
Ruky8821 <rumena78@gmail.com>
2023-04-27 22:08:11 -0700

    10-print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,12 +17,15 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
-			for (j = 1; j <= i; j++)
+			for (j = 1; j <= size; j++)
 			{
+				if ((i + j) <= size)
+					_putchar(' ');
+				else
 				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 
 
