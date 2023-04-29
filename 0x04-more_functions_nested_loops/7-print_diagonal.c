8adcbd90b8e8eb28eefe20ea6f47b386bbc07481
Ruky8821 <rumena78@gmail.com>
2023-04-29 00:07:53 -0700

    7-print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,21 +11,20 @@ void print_diagonal(int n)
 {
 	int x, y;
 
-	if (n <= 0)
+	if (n <= 1)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for (y = 1; y <= x; y++)
+			for (y = 0;  y < x; y++)
 			{
-				_putchar(' ');
+				_putchar(32);
 			}
 			_putchar(92);
 			_putchar('\n');
 		}
-		
 	}
 }
