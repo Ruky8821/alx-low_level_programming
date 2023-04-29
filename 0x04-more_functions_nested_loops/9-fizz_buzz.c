e44d0da93f3fb911e0a39608e4ad81e8c36a3c1a
Ruky8821 <rumena78@gmail.com>
2023-04-29 08:54:00 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("fizzBuzz");
+		}
+		else if (n % 3 == 0)
 		{
 			printf("fizz");
 		}
 		else if (n % 5 == 0)
 		{
 			printf("buzz");
-		}
-		else if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("fizzBuzz");
 		}
 		else
 		{
