fb05290440dc3cfc1d10d948c8b0acd1e0624090
Ruky8821 <rumena78@gmail.com>
2023-04-30 16:19:01 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *Return: 0 success
+ */
+
+
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (i == 4 || i == 16)
+			continue;
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
