679b9f8c1803f4d6796fa9fa85464fc0f387cdbf
Ruky8821 <rumena78@gmail.com>
2023-04-30 16:33:15 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+	{
+		printf("%d\n", p);
+	}
+	printf("\n");
+	return (0);
+}
