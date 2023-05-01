c2f6c96233f67b13d52c7c5f59e91293e47ad3b6
Ruky8821 <rumena78@gmail.com>
2023-04-30 17:48:57 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 8-print_base16.c -o 8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 9; p++)
+	{
+		putchar(p + 48);
+	}
+	for (q = 97; q <= 101; q++)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+	return (0);
+}
