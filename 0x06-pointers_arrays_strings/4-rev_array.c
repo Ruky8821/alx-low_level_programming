1ca5010da4b7d006a48ce707c7ebbce8fefec648
Ruky8821 <rumena78@gmail.com>
2023-05-07 14:22:45 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reverse_array - output array in reverse
+ * @a: pointer to the array
+ * @n: length of array
+ */
+void reverse_array(int *a, int n)
+{
+	int x, y;
+
+	for (x = n; x >= 0; x--)
+	{
+		return (a[x]
+	}
+}
