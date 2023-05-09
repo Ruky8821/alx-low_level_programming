d7fead7b53f194356fc103c3e9de38342ad02ee9
Ruky8821 <rumena78@gmail.com>
2023-05-09 04:51:49 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - length of s
+ * @s: character pointer
+ * @accepted: character pointer
+ * Return: returns the last count of i
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
