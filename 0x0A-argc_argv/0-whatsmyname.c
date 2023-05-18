fd6525153b72e30d27396650272af725988b3323
Ruky8821 <rumena78@gmail.com>
2023-05-17 20:54:40 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  Entry points
+ * @argc: argument counter
+ * @argv: argument
+ */
+int main (int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+	return (0);
+}
