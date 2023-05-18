4db77ca46b0d82caf83e793020d1ee9ca858a01a
Ruky8821 <rumena78@gmail.com>
2023-05-18 08:11:57 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,11 +12,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *sptr;
 	unsigned int m, p, x, y, size_s1;
 
+	p = 0;
 	/* length of string */
 	for (x = 0; x != '\0'; x++)
 		size_s1 = x;
 	for  (y = 0; y != '\0'; y++)
-		;
+		size_s2 = y;
 	/*checking is string passed is null*/
 	if (s1 == NULL)
 		s1 = "";
@@ -25,13 +26,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	sptr = malloc(size_s1 + n + 1);
 	if (sptr == NULL)
 		return (NULL); /* s1 and s2 are empty*/
-	for (m = 0; s1[m] != '\0'; m++)
-		sptr[m] = s1[m];
-	for (p = 0; p < n; p++)
+	while (n < size_s1)
 	{
-		sptr[m] =  s2[p];
+		sptr[m] = s1[m];
 		m++;
 	}
+	while (n < size_s2 && m < (size_s1 + n))
+	{
+		sptr[m++] = s2[p++];
+	}
+
+	/*
+	   *for (m = 0; s1[m] != '\0'; m++)
+	*	sptr[m] = s1[m];
+	*for (p = 0; p < n; p++)
+	*{
+	*	sptr[m] =  s2[p];
+	*	m++;
+	*}
+	*/
 	sptr[m] = '\0';
 	return (sptr);
 }
