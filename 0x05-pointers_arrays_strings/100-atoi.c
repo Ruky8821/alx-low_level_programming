b0a8b41ac5bba676c8acd8708e023b35cad9d69a
Ruky8821 <rumena78@gmail.com>
2023-05-21 07:59:08 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,43 +6,17 @@
  */
 int _atoi(char *s)
 {
-	int i, num, j, k, l, dgt;
+	int x;
+	unsigned int i,j;
 
-	i = 0;
-	num = 0;
-	j = 0;
-	k = 0;
-	l = 0;
-	dgt = 0;
-
-	if (s[i] != '\0')
-	{
-		i++;
-	}
-	while (num < i && j == 0)
-	{
-		if (s[num] == '-')
-		{
-			++k;
-		}
-		if (s[num] >= '0' && s[num] <= '9')
-		{
-			dgt = s[num] - '0';
-			if (k % 2)
-			{
-				dgt =  -dgt;
-			}
-			l = l * 10 + dgt;
-			j = 1;
-			if (s[num + 1] < 48 || s[num + 1] > 57)
-				break;
-			j = 0;
-		}
-		num++;
-	}
-	if (j == 0)
-	{
-		return (0);
-	}
-	return (l);
+	do {
+		x = 1;
+		if (s[j] == '_')
+			x = x * (-1);
+		else if  (i > 0)
+			break;
+		else if (s[j] >= '0' && s[j] <= '9')
+			i = (i * 10) + (s[j] - '0');
+	} while (s[j++]);
+	return (x * i);
 }
