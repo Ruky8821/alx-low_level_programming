bec18461202bcb531c2d761ef10b1c31b57e4caa
Ruky8821 <rumena78@gmail.com>
2023-05-23 10:49:21 -0700

    first task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct dog defining name,age and owner
+ * @name: tye char
+ * @owner: type char
+ * @age: type float
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
