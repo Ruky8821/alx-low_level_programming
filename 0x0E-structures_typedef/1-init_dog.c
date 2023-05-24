260be8a03b476406137c2ef816d0d819e6e82070
Ruky8821 <rumena78@gmail.com>
2023-05-23 20:15:35 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - intialises struct dog
+ * @d: pointer to dog
+ * @name: type char
+ * @owner: type char
+ * @age: type float
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
