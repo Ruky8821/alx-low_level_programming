afcfc0d48c447ddc5e5208042fa0ace0b9165de9
Ruky8821 <rumena78@gmail.com>
2023-06-01 15:18:28 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,13 +13,18 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 
 	if (!new)
+	{
+		free (new);
 		return (NULL);
+	}
 	if (str)
 		new->str = strdup(str);
 	else
 		return (NULL);
+
 	while (str[len])
 		len++;
+
 	new->len = len;
 	new->next = NULL;
 
@@ -32,3 +37,4 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	return (*head);
 }
+
