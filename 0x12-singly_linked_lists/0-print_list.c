dd841fb5f3f356e3e441021ff1f3c4d1dff89999
Ruky8821 <rumena78@gmail.com>
2023-06-01 07:35:39 -0700

    task 0 printing elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list -  prints content and lenght of the node
+ * @h: a pointer to the next
+ * Return: i on success
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str ==  NULL)
+			printf("[0]\n");
+		else
+		{
+			printf("%s[%d]\n", h->str, h->len);
+			i++;
+		}
+	}
+	return (i);
+
+}
