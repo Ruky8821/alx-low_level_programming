e2072abcf4dcc2c8dfaf42b54e6fb79402e8650e
Ruky8821 <rumena78@gmail.com>
2023-06-01 09:59:50 -0700

    putchar added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,15 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	for (; h != NULL; h = h->next)
+	while (h != NULL)
 	{
 		if (h->str ==  NULL)
 			printf("[0]\n");
 		else
 		{
-			printf("[%u]%s\n", h->len, h->str);		
+			printf("[%u]%s\n", h->len, h->str);
 		}
+		h = h->next;
 		i++;
 	}
 	return (i);
