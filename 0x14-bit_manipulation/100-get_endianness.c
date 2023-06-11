3a6df147c1f0383b6544c75f01b886430d6aaa1e
Ruky8821 <rumena78@gmail.com>
2023-06-11 07:46:59 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,10 +7,7 @@
 int get_endianness(void)
 {
 	unsigned int val = 0x12345;
-	unsigned char *byte = (unsigned char *) &val;
+	unsigned char *byte = (unsigned char *)&val;
 
-	if (*byte == 0x45)
-		return (1);
-	else
-		return (0);
+	return (*byte);
 }
