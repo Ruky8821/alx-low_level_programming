9f82799d2b59465d69e9e4de2cfe15b6e2c9a756
Ruky8821 <rumena78@gmail.com>
2023-06-11 07:41:13 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - determines if littel endian or big endian
+ * Return: 1 for little endian and 0 for big endian
+ *
+ */
+int get_endianness(void)
+{
+	unsigned int val = 0x12345;
+	unsigned char *byte = (unsigned char *) &val;
+
+	if (*byte == 0x45)
+		return (1);
+	else
+		return (0);
+}
