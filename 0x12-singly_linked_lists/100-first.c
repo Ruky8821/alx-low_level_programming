ed34b046037fd6a1c1923084e602885e038efd71
Ruky8821 <rumena78@gmail.com>
2023-06-22 10:38:36 +0200

    on bash'

--- 0x12-singly_linked_lists/100-first.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void print_first(void) __attribute__((constructor));
-/**
- * print_first - prints before the main function
- */
-void print_first(void)
-{
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
