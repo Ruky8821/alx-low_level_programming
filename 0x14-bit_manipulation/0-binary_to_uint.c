2d0b38d17c3a171ef94a45656ed09b77c7fd9652
Ruky8821 <rumena78@gmail.com>
2023-08-03 00:30:41 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary stirng to decimal
+ * @b: character string
+ * Return: result on success or 0 if it fails
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, power = 0, idx = 0, j = 0, i = 0;
+	size_t len = 0;
+
+	if (!b)
+		return (0);
+
+	while (b[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			power = len - i - 1;
+			idx = 1;
+
+			for (j = 0; j < power; j++)
+				idx *= 2;
+			result += (b[i] - '0') * idx;
+		}
+		else
+			return (0);
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a readme file for 0x14 bit manipulation task. ALX month 2
