6b7e8e07068226e2422d755786fab66d15e8360e
Ruky8821 <rumena78@gmail.com>
2023-11-09 06:15:47 -0800

    t0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x;
+
 	x = 0;
 
 	if (h == NULL)
 		return (0);
+	while (h->prev != NULL)
+		h = h->prev;
 	while (h)
 	{
 		printf("%d \n", h->n);
